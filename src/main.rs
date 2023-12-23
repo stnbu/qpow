1c1ce3ff6c1c9e257925f00fadb25f2cab2a6cd0
Mike Burr <mb@unintuitive.org>
2023-12-23 11:14:03 -0700

    .

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
+use recursion::*;
+
 enum Expr {
     Pow(Box<Expr>, Box<Expr>),
-    Int(u32),
+    Literal(u32),
 }
 fn pow(a: Expr, b: Expr) -> Expr {
     Expr::Pow(Box::new(a), Box::new(b))
 }
 fn int(n: u32) -> Expr {
-    Expr::Int(n)
+    Expr::Literal(n)
 }
 
 pub enum ExprFrame<A> {
     Pow(A, A),
-    Int(u32),
+    Literal(u32),
 }
-use recursion::*;
+
 impl MappableFrame for ExprFrame<PartiallyApplied> {
     type Frame<X> = ExprFrame<X>;
     fn map_frame<A, B>(input: Self::Frame<A>, mut f: impl FnMut(A) -> B) -> Self::Frame<B> {
         match input {
             ExprFrame::Pow(a, b) => ExprFrame::Pow(f(a), f(b)),
-            ExprFrame::Int(x) => ExprFrame::Int(x),
+            ExprFrame::Literal(x) => ExprFrame::Literal(x),
         }
     }
 }
@@ -28,14 +30,14 @@ impl<'a> Collapsible for &'a Expr {
     fn into_frame(self) -> <Self::FrameToken as MappableFrame>::Frame<Self> {
         match self {
             Expr::Pow(a, b) => ExprFrame::Pow(a, b),
-            Expr::Int(x) => ExprFrame::Int(*x),
+            Expr::Literal(x) => ExprFrame::Literal(*x),
         }
     }
 }
 fn eval(e: &Expr) -> u32 {
     e.collapse_frames(|frame: ExprFrame<u32>| match frame {
         ExprFrame::Pow(a, b) => a.pow(b),
-        ExprFrame::Int(x) => x,
+        ExprFrame::Literal(x) => x,
     })
 }
 
