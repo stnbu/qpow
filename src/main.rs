e8f8591e5c69e1d62a98de87768f384b68894613
Mike Burr <mb@unintuitive.org>
2023-12-23 10:54:30 -0700

    shrug -_-

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,24 @@
 enum Expr {
-    Add(Box<Expr>, Box<Expr>),
-    Sub(Box<Expr>, Box<Expr>),
-    Mul(Box<Expr>, Box<Expr>),
-    LiteralInt(i64),
+    Pow(Box<Expr>, Box<Expr>),
+    LiteralInt(u32),
 }
-fn add(a: Expr, b: Expr) -> Expr {
-    Expr::Add(Box::new(a), Box::new(b))
+fn pow(a: Expr, b: Expr) -> Expr {
+    Expr::Pow(Box::new(a), Box::new(b))
 }
-fn subtract(a: Expr, b: Expr) -> Expr {
-    Expr::Sub(Box::new(a), Box::new(b))
-}
-fn multiply(a: Expr, b: Expr) -> Expr {
-    Expr::Mul(Box::new(a), Box::new(b))
-}
-fn literal(n: i64) -> Expr {
+fn literal(n: u32) -> Expr {
     Expr::LiteralInt(n)
 }
 
 pub enum ExprFrame<A> {
-    Add(A, A),
-    Sub(A, A),
-    Mul(A, A),
-    LiteralInt(i64),
+    Pow(A, A),
+    LiteralInt(u32),
 }
 use recursion::*;
 impl MappableFrame for ExprFrame<PartiallyApplied> {
     type Frame<X> = ExprFrame<X>;
     fn map_frame<A, B>(input: Self::Frame<A>, mut f: impl FnMut(A) -> B) -> Self::Frame<B> {
         match input {
-            ExprFrame::Add(a, b) => ExprFrame::Add(f(a), f(b)),
-            ExprFrame::Sub(a, b) => ExprFrame::Sub(f(a), f(b)),
-            ExprFrame::Mul(a, b) => ExprFrame::Mul(f(a), f(b)),
+            ExprFrame::Pow(a, b) => ExprFrame::Pow(f(a), f(b)),
             ExprFrame::LiteralInt(x) => ExprFrame::LiteralInt(x),
         }
     }
@@ -39,28 +27,21 @@ impl<'a> Collapsible for &'a Expr {
     type FrameToken = ExprFrame<PartiallyApplied>;
     fn into_frame(self) -> <Self::FrameToken as MappableFrame>::Frame<Self> {
         match self {
-            Expr::Add(a, b) => ExprFrame::Add(a, b),
-            Expr::Sub(a, b) => ExprFrame::Sub(a, b),
-            Expr::Mul(a, b) => ExprFrame::Mul(a, b),
+            Expr::Pow(a, b) => ExprFrame::Pow(a, b),
             Expr::LiteralInt(x) => ExprFrame::LiteralInt(*x),
         }
     }
 }
-fn eval(e: &Expr) -> i64 {
-    e.collapse_frames(|frame| match frame {
-        ExprFrame::Add(a, b) => a + b,
-        ExprFrame::Sub(a, b) => a - b,
-        ExprFrame::Mul(a, b) => a * b,
+fn eval(e: &Expr) -> u32 {
+    e.collapse_frames(|frame: ExprFrame<u32>| match frame {
+        ExprFrame::Pow(a, b) => a.pow(b),
         ExprFrame::LiteralInt(x) => x,
     })
 }
 
 fn main() {
-    let expr = add(
-        multiply(subtract(literal(1), literal(2)), literal(3)),
-        literal(1),
-    );
-    assert_eq!(eval(&expr), -2);
+    let expr = pow(literal(2), literal(2));
+    assert_eq!(eval(&expr), 4);
 }
 /*
 
