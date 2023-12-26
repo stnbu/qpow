4ffd35a0217c272e509665329e230eafbf717b04
Mike Burr <mb@unintuitive.org>
2023-12-26 13:13:19 -0700

    old --> new

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,67 @@
-use num_traits::*;
-/*
-struct ExtendedInt {
-    integer: Option<u32>,
-    root: Option<(u32, u32)>,
+enum Expr {
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    LiteralInt(i64),
 }
- */
-
-enum ExtendedInt {
-    Int(u32),
-    Root((u32, u32)),
-    IExpr((Box<ExtendedInt>, Box<ExtendedInt>, Operation)),
+fn add(a: Expr, b: Expr) -> Expr {
+    Expr::Add(Box::new(a), Box::new(b))
 }
-enum Operation {
-    Mul,
-    Ident,
+fn subtract(a: Expr, b: Expr) -> Expr {
+    Expr::Sub(Box::new(a), Box::new(b))
 }
-
-struct Expr {
-    a: ExtendedInt,
-    b: ExtendedInt,
-    oper: Operation,
+fn multiply(a: Expr, b: Expr) -> Expr {
+    Expr::Mul(Box::new(a), Box::new(b))
+}
+fn literal(n: i64) -> Expr {
+    Expr::LiteralInt(n)
 }
 
-fn eval(expr: Expr) -> ExtendedInt {
-    match expr {
-        Expr {
-            a: _,
-            b: _,
-            oper: Operation::Mul,
-        } => ExtendedInt::Int(777),
-        _ => ExtendedInt::Int(666),
+pub enum ExprFrame<A> {
+    Add(A, A),
+    Sub(A, A),
+    Mul(A, A),
+    LiteralInt(i64),
+}
+use recursion::*;
+impl MappableFrame for ExprFrame<PartiallyApplied> {
+    type Frame<X> = ExprFrame<X>;
+    fn map_frame<A, B>(input: Self::Frame<A>, mut f: impl FnMut(A) -> B) -> Self::Frame<B> {
+        match input {
+            ExprFrame::Add(a, b) => ExprFrame::Add(f(a), f(b)),
+            ExprFrame::Sub(a, b) => ExprFrame::Sub(f(a), f(b)),
+            ExprFrame::Mul(a, b) => ExprFrame::Mul(f(a), f(b)),
+            ExprFrame::LiteralInt(x) => ExprFrame::LiteralInt(x),
+        }
     }
+}
+impl<'a> Collapsible for &'a Expr {
+    type FrameToken = ExprFrame<PartiallyApplied>;
+    fn into_frame(self) -> <Self::FrameToken as MappableFrame>::Frame<Self> {
+        match self {
+            Expr::Add(a, b) => ExprFrame::Add(a, b),
+            Expr::Sub(a, b) => ExprFrame::Sub(a, b),
+            Expr::Mul(a, b) => ExprFrame::Mul(a, b),
+            Expr::LiteralInt(x) => ExprFrame::LiteralInt(*x),
+        }
+    }
+}
+fn eval(e: &Expr) -> i64 {
+    e.collapse_frames(|frame| match frame {
+        ExprFrame::Add(a, b) => a + b,
+        ExprFrame::Sub(a, b) => a - b,
+        ExprFrame::Mul(a, b) => a * b,
+        ExprFrame::LiteralInt(x) => x,
+    })
 }
 
 fn main() {
-    let _1 = ExtendedInt::Int(1);
-    let _2 = ExtendedInt::Int(2);
-    let e = Expr {
-        a: _1,
-        b: _2,
-        oper: Operation::Mul,
-    };
-    let r = eval(e);
+    let expr = add(
+        multiply(subtract(literal(1), literal(2)), literal(3)),
+        literal(1),
+    );
+    assert_eq!(eval(&expr), -2);
 }
+/*
+
+*/
