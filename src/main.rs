e13086221bae27c6c60aa1cf4104f5a90857949c
Mike Burr <mb@unintuitive.org>
2023-12-28 13:24:32 -0700

    .

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fmt;
 
+use num_integer::Roots;
+
 enum Expr<'a> {
     Int(u32),
     Root(u32, u32),
@@ -11,13 +13,14 @@ impl fmt::Display for Expr<'_> {
         match self {
             Self::Int(i) => write!(f, "{i}"),
             Self::Root(m, p) => write!(f, "{m}^(1/{p})"),
-            Self::Op(op) => write!(f, "{op}"),
+            Self::Op(op) => write!(f, "({op})"),
         }
     }
 }
 
 enum Operation<'a> {
     Add(&'a Expr<'a>, &'a Expr<'a>),
+    Mul(&'a Expr<'a>, &'a Expr<'a>),
 }
 
 impl fmt::Display for Operation<'_> {
@@ -25,6 +28,9 @@ impl fmt::Display for Operation<'_> {
         match self {
             Self::Add(a, b) => {
                 write!(f, "{a} + {b}")
+            }
+            Self::Mul(a, b) => {
+                write!(f, "{a} × {b}")
             }
         }
     }
@@ -37,7 +43,12 @@ fn main() {
 
     let root2 = Expr::Root(2, 2);
     let wat = add(&three, &root2);
-    println!("{wat}");
+
+    let root3 = Expr::Root(3, 2);
+    let wot = add(&three, &root3);
+
+    let xx = mul(&wat, &wot);
+    println!("{xx}");
 }
 
 fn add<'t>(a: &'t Expr<'t>, b: &'t Expr<'t>) -> Expr<'t> {
@@ -45,5 +56,13 @@ fn add<'t>(a: &'t Expr<'t>, b: &'t Expr<'t>) -> Expr<'t> {
     match (a, b) {
         (Int(a), Int(b)) => Int(a + b),
         (a, b) => Op(Box::new(Operation::Add(&a, &b))),
+    }
+}
+
+fn mul<'t>(a: &'t Expr<'t>, b: &'t Expr<'t>) -> Expr<'t> {
+    use Expr::*;
+    match (a, b) {
+        (Int(x), Int(y)) => Int(x * y),
+        _ => Op(Box::new(Operation::Mul(&a, &b))),
     }
 }
